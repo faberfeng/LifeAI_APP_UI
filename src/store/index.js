5b1dc6b4587416eb66bfb48fbde88cefe736327e
faberfeng <992068956@qq.com>
2025-11-10 22:51:35 +0800

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import {defineStore} from "pinia";
 export const useAppStore = defineStore('app', {
     state: () => ({
         activeTab:0,
-        title:'这是一个标题',
+        title:'LifeAI',
     }),
     actions: {
     }
